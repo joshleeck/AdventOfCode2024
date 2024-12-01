c3b29500396f081d84ad85f84629a298e67946e3
joshleeck <32456936+joshleeck@users.noreply.github.com>
2024-12-02 03:01:10 +0800

    Solved Day 1 Puzzle

---
+++ Day1.py
@@ -0,0 +1,32 @@
+if __name__ == "__main__":
+    # Read shared file
+    filename = 'C:/Users/User/PycharmProjects/AdventOfCode2024/Data_Day1.txt'
+
+    with open(filename) as file:
+        list_a = []
+        list_b = []
+        for line in file:
+            data_line = line.rstrip().split(' ')
+            list_a.append(int(data_line[0]))
+            list_b.append(int(data_line[3]))
+
+    # Part 1
+    list_a = sorted(list_a)
+    list_b = sorted(list_b)
+
+    diff_sum = 0
+    for i in range(len(list_a)):
+        if list_a[i] >= list_b[i]:
+            diff_sum += list_a[i] - list_b[i]
+        else:
+            diff_sum += list_b[i] - list_a[i]
+
+    print(diff_sum) # Correct
+
+    # Part 2
+    sim_sum = 0
+    for i in range(len(list_a)):
+        sim_sum += list_a[i] * list_b.count(list_a[i])
+
+    print(sim_sum) # Correct
+
